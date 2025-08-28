6e900615824d267c2aba4e2859fd64794e2d37d1
joshjms <joshjms1607@gmail.com>
2025-08-28 10:41:20 +0800

    worker: Add health checks
    Signed-off-by: joshjms <joshjms1607@gmail.com>

--- worker/main.go
+++ worker/main.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/joshjms/invoker/api/workerpb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 type workerServer struct {
@@ -31,14 +33,18 @@ func (s *workerServer) Work(ctx context.Context, req *workerpb.WorkRequest) (*wo
 }
 
 func main() {
-	listener, err := net.Listen("tcp", ":50051")
+	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Fatalf("failed to listen at port 50051: %v", err)
+		log.Fatalf("failed to listen at port 8080: %v", err)
 	}
 
 	server := grpc.NewServer()
 	workerpb.RegisterWorkerServiceServer(server, &workerServer{})
 
+	healthSrv := health.NewServer()
+	healthpb.RegisterHealthServer(server, healthSrv)
+
+	log.Println("worker running at port 8080")
 	if err := server.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
