c6206abb2f21b285a7fd33f6398bfc04845b855d
joshjms <joshjms1607@gmail.com>
2025-08-29 00:02:05 -0600

    deploy with grpc port mapping
    Signed-off-by: joshjms <joshjms1607@gmail.com>

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -4,12 +4,13 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
-	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 )
 
@@ -21,22 +22,32 @@ var deployCmd = &cobra.Command{
 }
 
 func FuncDeploy(cmd *cobra.Command, args []string) {
-	if len(args) < 1 {
-		log.Fatalf("required 1 argument\n")
+	if len(args) < 2 {
+		log.Fatalf("required 2 arguments\n")
 	}
 
-	svcName := uuid.NewString()
-	image := args[0]
+	svcName := args[0]
+	image := args[1]
 
 	log.Printf("Running %s, service: %s\n", image, svcName)
 
-	deploySvcCmd := exec.Command("kn", "service", "create", svcName, fmt.Sprintf("--image=%s", image))
-	deploySvcCmd.Stdout = os.Stdout
+	outBuf := &bytes.Buffer{}
+
+	deploySvcCmd := exec.Command("kn", "service", "create", svcName, fmt.Sprintf("--image=%s", image), "--port=h2c:8080")
+	deploySvcCmd.Stdout = outBuf
 	deploySvcCmd.Stderr = os.Stderr
 
 	if err := deploySvcCmd.Run(); err != nil {
 		log.Fatalf("failed to deploy service: %v\n", err)
 	}
+
+	outStr := outBuf.String()
+	httpEndpoint := strings.Split(outStr, "\n")[len(strings.Split(outStr, "\n"))-2]
+	grpcEndpoint := strings.ReplaceAll(httpEndpoint, "http://", "")
+	grpcEndpoint += ":80"
+
+	fmt.Printf("\nDeployed service %s at %s\n", svcName, httpEndpoint)
+	fmt.Printf("You can now run:\n\n\tinvoker run %s\n\n", grpcEndpoint)
 }
 
 func init() {
